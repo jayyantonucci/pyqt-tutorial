faa7fb459c8121cf7097ef09a32a2cb223cd38c9
jayyantonucci <jayyantonucci@gmail.com>
2021-03-22 13:59:13 -0400

    button-events commit

--- tutorial1.py
+++ tutorial1.py
@@ -2,16 +2,34 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("window title")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("my first label")
+        self.label.move(50,50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("you clicked it!")
+        self.update()
+
+    def update(self):
+       self.label.adjustSize()
+
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("window title")
+    win = MyWindow()
 
-    label = QtWidgets.QLabel(win)
-    label.setText("my first label")
-    label.move(50,50)
-    
 
     win.show()
     sys.exit(app.exec_())
